70ed6f34728625ab44fd95a115d0ee9e1d905bec
Kosynskyi <kosynskyi.vova@gmail.com>
2023-02-26 17:35:32 +0200

    init hw03

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Volodymyr:PzzUcH4VhuBYdvex@cluster0.392t2gl.mongodb.net/db_contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect success"))
+  .catch((error) => console.log(error.message));
+
+app.listen(3001, () => {
+  console.log("Server running. Use our API on port: 3001");
+});
